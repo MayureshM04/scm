9125a342dc6fa19b2662eb2a1eb5de4afb0417bd
MayureshM04 <58592864+MayureshM04@users.noreply.github.com>
2019-12-07 00:20:57 +0530

    Update assessment1.c

--- assessment1.c
+++ assessment1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-int marr[3][];
-nrows=3,ncols=5;
+int marr[3][5];
+int nrows=3,ncols=5;
 int main() {
 for(int i=0;i<nrows;i++)
 {
@@ -9,7 +9,7 @@ for(int i=0;i<nrows;i++)
      marr[i][j]=rand()%100;  
   }
 }
-int column_wise_sum[ncols],total;
+int column_wise_sum[ncols];
 for(int j=0;j<ncols;j++)
 {
   column_wise_sum[j]=0;
@@ -19,7 +19,7 @@ for(int j=0;j<ncols;j++)
     column_wise_sum[j]+=marr[i][j];
   }
 
-  total+=column_wise_sum[j];
+  int total+=column_wise_sum[j];
   printf("\n");
 }
   return 0;
